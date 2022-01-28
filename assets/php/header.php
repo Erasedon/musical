7993318f5f2d6d29c749ce095c17428d7d287c28
erwantaupin <taupin.erwan@laposte.net>
2022-01-28 22:16:00 +0100

    1er save

---
+++ assets/php/header.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Header</title>
+</head>
+<body>
+    <div class="header">
+        <!--<img src="assets/img/header.jpg" alt="Header">-->
+        <nav class="navbar dark-mode" role="navigation">
+    <div class="navbar__logo">Logo</div>    
+    <ul class="navbar__links">
+      <li class="navbar__link first"><a href="#">Accueil</a></li>
+      <li class="navbar__link second"><a href="#">Services</a></li>
+      <li class="navbar__link third"><a href="#">Missions</a></li>
+      <li class="navbar__link four"><a href="#">Portfolio</a></li>
+      <li class="navbar__link fifth"><a href="#">Contact</a></li>
+    </ul>   
+    <button class="burger">
+      <span class="bar"></span>  
+    </button>   
+  </nav>
+  <main class="main-content" role="main">
+    <h1>Menu Responsive avec Burger Animé </h1>
+  </main>
+    </div>
+</body>
+</html>
