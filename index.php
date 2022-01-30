f6f2e0b3753953449d0d9fbc64dfaec26eaab371
yanis.pirlet <yanis.pirlet@gmail.com>
2022-01-30 23:52:17 +0100

    idéebase

---
+++ index.php
@@ -0,0 +1,25 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="screen" type="text/css" href="assets/css/style.css"/>
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+
+    include "assets/php/artiste.php";
+
+    ?>
+    <?php 
+
+    include "assets/php/carousel.php";
+
+    ?>
+    <script src="assets/js/script.js" async></script>
+</body>
+</html>
