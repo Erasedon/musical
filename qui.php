aaaefa01dff3b8f1d51f1ff2113c5957e7b9c1eb
BadbooX <k.cotton.pro@gmail.com>
2022-01-31 14:41:09 +0100

    commit kéké

---
+++ qui.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="assets/js/script.js" async></script>
+    <title>qui somme-nous</title>
+</head>
+<body>
+    <?php include "assets/php/header.php" ?>
+    <div class="presentation">
+    <p>Black label, est un site de streaming pour écouté votre music où vous le voulez</p>
+    </div>
+    <?php include "assets/php/footer.php" ?>
+</body>
+</html>
